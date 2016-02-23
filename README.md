a699877b43733b7bac40aefdf7690645ebad458d
Lucas Lima Muller <contato@lucaslimamuller.com.br>
2016-02-23 19:17:19 +0000

    Challenge finished

---
+++ README.md
@@ -0,0 +1,7 @@
+# Request Header Parser Microservice
+
+This is my solution to the challenge `Request Header Parser Microservice` on [Free Code Camp]("http://www.freecodecamp.com/").
+
+## What should it do?
+
+**User Story:** I can get the IP address, language and operating system for my browser.
---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var express = require("express");
+var app = new express();
+
+app.get("/", function(req,res){
+  var os = req.headers["user-agent"].split("(")[1].split(")")[0];
+  
+  var result = {
+    "ipaddress" : req.headers["x-forwarded-for"],
+    "language": req.acceptedLanguages[0],
+    "software":os
+  };
+  
+  res.end(JSON.stringify(result));
+});
+
+app.listen(8080);
